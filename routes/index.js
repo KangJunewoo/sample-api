b8082bd649e66d678fe42c80bfb0b0c4f0c7dd5d
KangJunewoo <brianjune.dev@gmail.com>
2021-03-08 00:47:19 +0900

    feat: for stress test

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const util = require('../modules/util');
-const responseMessage = require('../modules/responseMessage');
-const statusCode = require('../modules/statusCode');
+const rm = require('../modules/responseMessage');
+const sc = require('../modules/statusCode');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+    return res.status(sc.OK).send(util.success(sc.OK, rm.OK, 'GET 요청 성공'));
 });
 
-router.get('/desktop', (req,res)=>{
-  data = []
-  for(let i=0;i<20;i++) data.push({'download_url':'http://placeimg.com/1024/576'})
-  res.status(statusCode.OK).send(data);
-});
-
-router.get('/mobile', (req,res)=>{
-  data = []
-  for(let i=0;i<20;i++) data.push({'download_url':'http://placeimg.com/360/640'})
-  res.status(statusCode.OK).send(data);
+router.post('/', (req, res) => {
+    return res
+        .status(sc.OK)
+        .send(util.success(sc.OK, rm.OK, `POST 요청 성공 : ${req.body.data}`));
 });
 
 module.exports = router;
